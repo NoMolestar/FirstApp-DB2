032974e24ad0267e111da5c9599b9045b754c749
Lalo Vene <lalohdez77@gmail.com>
2022-04-24 14:49:22 -0500

    Created db and backend endpoints

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # FirstApp-DB2
+
 First application with Frontend (React Native) and Backend (Flask) using DB2 (IBM) as DBMS.
+
+## Installation
+
+### Run DB
+
+```BASH
+docker pull ibmcom/db2
+docker run -itd --name mydb2 --privileged=true -p 50000:50000 -e LICENSE=accept -e DB2INST1_PASSWORD=<choose an instance password> -e DBNAME=testdb -v <db storage dir>:/database ibmcom/db2
+```
+
+### Run Flask
+
+```BASH
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # este es un comentario
 # 1ero - importar código necesario
-from tokenize import Double
 from flask import Flask, jsonify
 from markupsafe import escape
 from flask_db2 import DB2
-import sys
 from flask_cors import CORS
 import sqlalchemy
 from sqlalchemy import *
-import ibm_db_sa
 
 # 2do - creamos un objeto de tipo flask
 app = Flask(__name__)
@@ -25,93 +22,65 @@ db = DB2(app)
 
 CORS(app)
 
-# 3ero - al objeto de tipo flask le agregamos rutas
-# @ en python significa que vamos a usar un decorator
-# https://en.wikipedia.org/wiki/Decorator_pattern
-
 
 @app.route("/")
 def index():
     return "Hola mundo", 200
 
 
-@app.route("/selectAll")
-def selectAll():
+@app.route("/select")
+def select():
     try:
-        # lo primero es obtener cursor
-        cur = db.connection.cursor()
-
-        # con cursor hecho podemos ejecutar queries
-        cur.execute("SELECT * FROM productos")
-
-        # obtenemos datos
-        data = cur.fetchall()
-
-        # acuerdate de cerrar el cursor
-        cur.close()
-
-        print(data, file=sys.stdout)
-
-        # puedes checar alternativas para mapeo de datos
-        # por hoy vamos a armar un objeto jsoneable para regresar
-        resultado = []
-        for current in data:
+        engine = sqlalchemy.create_engine(
+            "ibm_db_sa://db2inst1:hola@localhost:50000/testdb")
+        conn = engine.connect()
+        result = conn.execute("SELECT id, name FROM products")
+        response = []
+        for current in result:
             actual = {
                 "id": current[0],
-                "nombre": current[1],
-                "precio": current[2]
+                "name": current[1],
             }
-            resultado.append(actual)
+            response.append(actual)
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-        return jsonify(resultado)
-    except:
-        return "<h1>Ocurrió un error al mostrar la tabla.</h1>"
 
-
-@app.route("/selectByID/<int:valor>", methods=["GET"])
-def selectByID(valor):
+@app.route("/select/<int:valor>", methods=["GET"])
+def selectID(valor):
     try:
-        # lo primero es obtener cursor
-        cur = db.connection.cursor()
-
-        # con cursor hecho podemos ejecutar queries
-        cur.execute("SELECT * FROM productos where id = ?", (escape(valor)))
-
-        # obtenemos datos
-        data = cur.fetchall()
-
-        # acuerdate de cerrar el cursor
-        cur.close()
-
-        print(data, file=sys.stdout)
-
-        # puedes checar alternativas para mapeo de datos
-        # por hoy vamos a armar un objeto jsoneable para regresar
-        resultado = []
-        for current in data:
+        engine = sqlalchemy.create_engine(
+            "ibm_db_sa://db2inst1:hola@localhost:50000/testdb")
+        conn = engine.connect()
+        result = conn.execute(
+            "SELECT * FROM products WHERE id = " + str(escape(valor)))
+        response = None
+        for current in result:
             actual = {
                 "id": current[0],
-                "nombre": current[1],
-                "precio": current[2]
+                "name": current[1],
+                "price": current[2],
             }
-            resultado.append(actual)
-
-        return jsonify(resultado)
-    except:
-        return "<h1>Ocurrió un error al mostrar la tabla.</h1>"
+            response = actual
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 # podemos tener todas las rutas
 
 
 @app.route("/createTable")
 def createTable():
-    # Create table products with id, name and price
-    cur = db.connection.cursor()
-    cur.execute("DROP TABLE IF EXISTS productos")
-    cur.execute(
-        "CREATE TABLE productos (id INTEGER NOT NULL, nombre VARCHAR(20), precio INTEGER)")
-    cur.close()
-    return "<h1>Tabla creada</h1>"
+    try:
+        engine = sqlalchemy.create_engine(
+            "ibm_db_sa://db2inst1:hola@localhost:50000/testdb")
+        engine.execute("DROP TABLE products IF EXISTS")
+        engine.execute(
+            "CREATE TABLE products (id INTEGER, name VARCHAR(20), price DOUBLE)")
+        return "Tabla creada", 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route("/insertValues")
@@ -120,73 +89,12 @@ def insertValues():
     try:
         engine = sqlalchemy.create_engine(
             "ibm_db_sa://db2inst1:hola@localhost:50000/testdb")
-        connection = engine.connect()
-        metadata = sqlalchemy.MetaData()
-        productos = Table('productos', metadata,
-                          Column('id', Integer, primary_key=True),
-                          Column('nombre', String(150), nullable=False),
-                          Column('precio', Double, nullable=False)
-                          )
-        query = productos.insert().values(
-            id=1, nombre='Leche', precio=10.0)
-        connection.execute(query)
-        query = productos.insert().values(
-            id=2, nombre='Cereal', precio=20.0)
-        connection.execute(query)
-        query = productos.insert().values(
-            id=3, nombre='Huevos', precio=30.0)
-        connection.execute(query)
-        query = productos.insert().values(
-            id=4, nombre='Tortillas', precio=22.0)
-        connection.execute(query)
-        query = productos.insert().values(
-            id=5, nombre='Frijoles', precio=350.0)
-        connection.execute(query)
-        return "<h1>Valores insertados con exito.</h1>"
-    except:
-        return "<h1>Ocurrió un error al insertar la tabla.</h1>"
-
-# podemos recibir variables a través de URL
-# f-string formatting
-# &eacute; & e acute; - e con acento agudo
-# ataque por inyección de código
-
-# @app.route("/nombre/<el_nombre>/<el_apellido>")
-# def nombre(el_nombre, el_apellido):
-#     return f"Hola {escape(el_nombre)} {escape(el_apellido)}, espero que est&eacute;s bien."
-
-# converters
-
-# @app.route("/entero/<int:valor>")
-# def entero(valor):
-#     return f"el valor que mandaste fue: {escape(valor)}"
-
-# @app.route("/ruta/<path:valor>")
-# def ruta(valor):
-#     return f"la ruta que mandaste fue: {escape(valor)}"
-
-# LO QUE VAMOS A REGRESAR GENERALMENTE VA A SER JSON
-# ventaja de flask - regresar un diccionario de python se jsonifica
-
-# diccionario = {
-#     "nombre": "Garfiol",
-#     "edad": 30,
-#     "peso": 64
-# }
-
-# @app.route("/json")
-# def json():
-#     return diccionario
-
-# se puede discriminar por medio de método de request de HTTP
-# (GET, POST, PUT, DELETE)
-
-# @app.route("/metodo", methods=["GET", "POST"])
-# def metodo_get_post():
-#     return "REQUEST HECHA POR GET O POST"
-
-# @app.route("/metodo", methods=["PUT", "DELETE"])
-# def metodo_put_delete():
-#     return "REQUEST HECHA POR PUT O DELETE"
-
-# EXTENSION DE VS CODE - THUNDERCLIENT
+        engine.execute("DELETE FROM products")
+        engine.execute("INSERT INTO products VALUES (1, 'Laptop', 1000)")
+        engine.execute("INSERT INTO products VALUES (2, 'Mouse', 10)")
+        engine.execute("INSERT INTO products VALUES (3, 'Keyboard', 20)")
+        engine.execute("INSERT INTO products VALUES (4, 'Monitor', 200)")
+        engine.execute("INSERT INTO products VALUES (5, 'Printer', 50)")
+        return "Valores insertados", 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
